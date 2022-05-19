cffd1e0d75c690de5622ae3375af2597fb24ee62
thiagoJoseB <79976791+cauamenezes@users.noreply.github.com>
2022-05-19 11:36:58 -0700

    recebendo as imagens enviadas pelo android

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true, limit: '5MB'}));
 
-app.post('./testeUpload', (req, res)=>{
+app.post('/testeUpload', (req, res)=>{
 
     let buffer = new Buffer.from(req.body.file, 'base64');
+    let imageName = './uploads/' + Date.now().toString() + '.jpg';
 
-    let imageName = './upload' + Date.now().toString() + '.jpg';
+    //console.log(req.body.file);
 
     fs.writeFileSync(imageName, buffer, 'base64', (error)=>{
 
         if(error) console.log(error);
     });
+
+    res.status(200);
 
 });
 
